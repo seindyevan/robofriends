a008b5bf88613e58734fbfacfa1ed9e57ce19fca
cloudywolfz <cloudywolfz@gmail.com>
2021-05-29 17:18:47 +0800

    pwa progress-added service worker

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { searchRobots, requestRobots } from './reducers';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 //create store(rootReducer) --> as there will be many reducer in large scale application
@@ -31,6 +32,14 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
